81d36664a795f47f119d4072115a7014da6f36c3
lenachat <124374456+lenachat@users.noreply.github.com>
2024-05-28 17:37:02 +0200

    added pokemonList array
    Added pokemonList array with Pokemon names and specifications
    
    - Introduced a new array named pokemonList to store Pokemon data.
    - Each object in the array contains Pokemon names, height and type.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-alert('Hello world');
-
-let favoriteFood = 'everything with Pasta';
-document.write('My favorite food is ' + favoriteFood)
+pokemonList = [
+  {
+    name: 'Bulbasaur',
+    height: 0.7,
+    types: ['grass', 'poison']
+  },
+  {
+    name: 'Charizard',
+    height: 1.7,
+    types: ['fire', 'flying']
+  },
+  {
+    name: 'Pidgey',
+    height: 0.3,
+    types: ['flying', 'normal']
+  },
+];
